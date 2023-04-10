694d6fa8c77fa6ab8628f43a1c785120f75cab65
sabinbaniya <dummymail@tutanota.de>
2023-04-10 21:58:30 +0545

    feat: add optional action prop to `SectionHeading`

--- src/components/common/SectionHeading/index.tsx
+++ src/components/common/SectionHeading/index.tsx
@@ -5,8 +5,11 @@ interface Props {
 
 const SectionHeading = ({ heading, action }: Props) => {
   return (
-    <div>
-      <p className="py-6 text-2xl font-bold">{heading}</p>
+    <div className="flex items-center justify-between">
+      <p className="py-6 text-3xl font-bold">{heading}</p>
+      {action && (
+        <button className="font-semibold text-indigo-600">{action}</button>
+      )}
     </div>
   );
 };
