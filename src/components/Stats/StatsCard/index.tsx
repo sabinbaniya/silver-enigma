f602b1d854234322f81f044d36028b340563deb5
sabinbaniya <dummymail@tutanota.de>
2023-04-11 08:37:16 +0545

    improve: `StatsCard` uses common `Card` component

--- src/components/Stats/StatsCard/index.tsx
+++ src/components/Stats/StatsCard/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Card from "../../common/Card";
 
 interface Props {
   src: string;
@@ -8,13 +9,15 @@ interface Props {
 
 const StatsCard = ({ src, label, value }: Props) => {
   return (
-    <div className="flex h-20 items-center space-x-1 rounded-lg border-2 border-gray-200/60 bg-gray-100 p-2">
-      <Image height={52} width={52} src={src} alt="" />
-      <div className="flex flex-col -space-y-1">
-        <span className="text-lg font-bold text-gray-950">{value}</span>
-        <span className="text-gray-700">{label}</span>
+    <Card>
+      <div className="flex h-20 items-center space-x-1">
+        <Image height={52} width={52} src={src} alt="" />
+        <div className="flex flex-col -space-y-1">
+          <span className="text-lg font-bold text-gray-950">{value}</span>
+          <span className="text-gray-700">{label}</span>
+        </div>
       </div>
-    </div>
+    </Card>
   );
 };
 
