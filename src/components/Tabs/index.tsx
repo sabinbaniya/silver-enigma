a7d13056f5b5b56992bf11973f663d7c3520150c
sabinbaniya <dummymail@tutanota.de>
2023-04-11 20:52:29 +0545

    refactor: use `TabsButton` component

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -1,13 +1,12 @@
+import { TabsValue } from "@/src/types/tabs";
+import TabsButton from "./TabsButton";
+
 const Tabs = () => {
   return (
     <>
       <div className="my-8 space-x-4 rounded-xl border border-gray-200 p-4">
-        <button className="rounded-lg bg-blue-100 p-2 font-semibold text-blue-800">
-          Portfolio
-        </button>
-        <button className="rounded-lg bg-gray-100 p-2 font-semibold text-gray-800">
-          Resume
-        </button>
+        <TabsButton text={TabsValue.PORTFOLIO} />
+        <TabsButton text={TabsValue.RESUME} />
       </div>
     </>
   );
