c508356a7ff4e909842b5aff7387adfc68fd0263
sabinbaniya <dummymail@tutanota.de>
2023-04-11 08:37:35 +0545

    improve: `ProjectsCard` uses common `Card` component

--- src/components/Projects/ProjectsCard/index.tsx
+++ src/components/Projects/ProjectsCard/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import LanguageLogoText from "./LanguageLogoText";
+import Card from "../../common/Card";
 
 interface Props {
   src: string;
@@ -9,23 +10,28 @@ interface Props {
 const ProjectsCard = ({ src, title }: Props) => {
   return (
     <>
-      <div className="rounded-xl border border-gray-200 bg-gray-100 p-4">
-        <Image
-          height={100}
-          width={500}
-          src={src}
-          alt=""
-          className="rounded-lg"
-        />
-        <div className="mt-4">
-          <p className="text-xl font-bold">{title}</p>
-          <div className="mt-2 flex items-center space-x-4">
-            <LanguageLogoText src="/assets/icons/html-5.svg" label="HTML/CSS" />
-            <span className="h-1.5 w-1.5 rounded-full bg-gray-500" />
-            <LanguageLogoText src="/assets/icons/react.svg" label="React" />
+      <Card>
+        <div className="p-4">
+          <Image
+            height={100}
+            width={500}
+            src={src}
+            alt=""
+            className="rounded-lg"
+          />
+          <div className="mt-4">
+            <p className="text-xl font-bold">{title}</p>
+            <div className="mt-2 flex items-center space-x-4">
+              <LanguageLogoText
+                src="/assets/icons/html-5.svg"
+                label="HTML/CSS"
+              />
+              <span className="h-1.5 w-1.5 rounded-full bg-gray-500" />
+              <LanguageLogoText src="/assets/icons/react.svg" label="React" />
+            </div>
           </div>
         </div>
-      </div>
+      </Card>
     </>
   );
 };
