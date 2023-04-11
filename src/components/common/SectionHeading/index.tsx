e11b2049833bc080770ca4becfb32549b6d8faeb
sabinbaniya <dummymail@tutanota.de>
2023-04-11 21:05:46 +0545

    feat: add `centered` prop to `SectionHeading` component

--- src/components/common/SectionHeading/index.tsx
+++ src/components/common/SectionHeading/index.tsx
@@ -1,11 +1,16 @@
 interface Props {
   heading: string;
   action?: string;
+  centered?: boolean;
 }
 
-const SectionHeading = ({ heading, action }: Props) => {
+const SectionHeading = ({ heading, action, centered }: Props) => {
   return (
-    <div className="flex items-center justify-between">
+    <div
+      className={`flex items-center ${
+        centered ? "justify-center" : "justify-between"
+      }`}
+    >
       <p className="py-6 text-3xl font-bold">{heading}</p>
       {action && (
         <button className="font-semibold text-indigo-600">{action}</button>
