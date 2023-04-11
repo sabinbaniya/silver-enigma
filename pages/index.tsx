bb1bdbe6519e4a68695a65d1c4cc74cb63ac437f
sabinbaniya <dummymail@tutanota.de>
2023-04-11 21:08:52 +0545

    feat: add tabs switching functionality & animation with `framer-motion`

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { activeTabAtom } from "@/src/atoms/activeTabAtom";
+import AboutMeSection from "@/src/components/AboutMe";
 import CertificatesSection from "@/src/components/Certificates";
 import Navbar from "@/src/components/Navbar";
 import PlaygroundsSection from "@/src/components/Playgrounds";
@@ -5,10 +7,14 @@ import Profile from "@/src/components/Profile";
 import ProjectsSection from "@/src/components/Projects";
 import StatsSection from "@/src/components/Stats";
 import Tabs from "@/src/components/Tabs";
+import { TabsValue } from "@/src/types/tabs";
+import { AnimatePresence, motion } from "framer-motion";
+import { useAtom } from "jotai";
 import type { NextPage } from "next";
 import Head from "next/head";
 
 const Home: NextPage = () => {
+  const [activeTab] = useAtom(activeTabAtom);
   return (
     <>
       <Head>
@@ -19,10 +25,53 @@ const Home: NextPage = () => {
         <main className="mx-auto max-w-4xl">
           <Profile />
           <Tabs />
-          <StatsSection />
-          <ProjectsSection />
-          <PlaygroundsSection />
-          <CertificatesSection />
+          <AnimatePresence>
+            {activeTab === TabsValue.PORTFOLIO && (
+              <motion.section
+                initial={{
+                  opacity: 0,
+                  scale: 0.95,
+                }}
+                animate={{
+                  opacity: 1,
+                  scale: 1,
+                }}
+                exit={{
+                  opacity: 0,
+                  scale: 0.95,
+                }}
+                transition={{
+                  duration: 0.2,
+                }}
+              >
+                <StatsSection />
+                <ProjectsSection />
+                <PlaygroundsSection />
+                <CertificatesSection />
+              </motion.section>
+            )}
+          </AnimatePresence>
+          <AnimatePresence>
+            {activeTab === TabsValue.RESUME && (
+              <motion.section
+                initial={{
+                  opacity: 0,
+                  scale: 0.95,
+                }}
+                animate={{
+                  opacity: 1,
+                  scale: 1,
+                }}
+                exit={{
+                  opacity: 0,
+                  scale: 0.95,
+                }}
+                transition={{ delay: 0.2 }}
+              >
+                <AboutMeSection />
+              </motion.section>
+            )}
+          </AnimatePresence>
         </main>
       </section>
     </>
