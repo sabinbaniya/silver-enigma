74872a981e44f58ac39129c4dd89e7289dcb3621
sabinbaniya <dummymail@tutanota.de>
2023-04-13 08:47:58 +0545

    new: create `SettingsLayout` component

---
+++ src/layout/SettingsLayout/index.tsx
@@ -0,0 +1,54 @@
+import Card from "@/src/components/common/Card";
+import { ReactNode } from "react";
+import { FaPortrait } from "react-icons/fa";
+import { HiUserCircle } from "react-icons/hi";
+import { IoIosPaper } from "react-icons/io";
+import { IoShareSocialSharp } from "react-icons/io5";
+import NavigationLink from "./NavigationLink";
+
+interface Props {
+  children: ReactNode;
+}
+
+const SettingsLayout = ({ children }: Props) => {
+  return (
+    <>
+      <div className="mx-auto flex w-11/12 max-w-7xl">
+        <aside className="h-min basis-1/5 pt-10">
+          <Card>
+            <div className="relative flex flex-col space-y-6 py-8 ">
+              <NavigationLink
+                href="/settings/profile"
+                icon={<HiUserCircle className="text-xl" />}
+              >
+                Profile
+              </NavigationLink>
+              <NavigationLink
+                href="/settings/socials"
+                icon={<IoShareSocialSharp className="text-lg" />}
+              >
+                Socials
+              </NavigationLink>
+              <NavigationLink
+                href="/settings/portfolio"
+                icon={<FaPortrait className="text-lg" />}
+              >
+                Portfolio
+              </NavigationLink>
+              <NavigationLink
+                href="/settings/resume"
+                icon={<IoIosPaper className="text-lg" />}
+              >
+                Resume
+              </NavigationLink>
+            </div>
+          </Card>
+        </aside>
+        <main className="-bg-gray-400 h-[200vh] basis-4/5 px-16 pt-10">
+          {children}
+        </main>
+      </div>
+    </>
+  );
+};
+export default SettingsLayout;
