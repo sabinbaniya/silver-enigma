e3bc3dc4381e6b78851087d6e73dff02c9f1fe02
sabinbaniya <dummymail@tutanota.de>
2023-04-13 19:36:17 +0545

    ui: use `useForm` for all `InputGroup`

--- pages/settings/profile.tsx
+++ pages/settings/profile.tsx
@@ -3,9 +3,33 @@ import InputGroup from "@/src/components/common/Forms/InputGroup";
 import { MainLayout, SettingsLayout } from "@/src/layout";
 import Head from "next/head";
 import Image from "next/image";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 interface Props {}
+
+interface FormFields {
+  name: string;
+  about: string;
+  profession: string;
+  dob: string;
+  gender: string;
+  test: string;
+}
+
 const ProfileSettings = (props: Props) => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormFields>();
+
+  console.log(watch("name"));
+
+  const onsubmit: SubmitHandler<FormFields> = (data) => {
+    console.log(data, "submitted");
+  };
+
   return (
     <>
       <Head>
@@ -26,8 +50,13 @@ const ProfileSettings = (props: Props) => {
                 <Button style="secondary">Delete</Button>
               </div>
             </div>
-            <div className="mt-8">
-              <InputGroup labelText="Display name">
+            <form onSubmit={handleSubmit(onsubmit)} className="mt-8">
+              <InputGroup
+                register={register}
+                errors={errors.name}
+                name="name"
+                labelText="Display name"
+              >
                 <p className="text-gray-500">
                   Name entered above will be used for all issued certificates
                 </p>
@@ -36,14 +65,34 @@ const ProfileSettings = (props: Props) => {
                 as="textarea"
                 labelText="About"
                 placeholder="Let people know something about you..."
+                register={register}
+                name="about"
+                errors={errors.about}
               />
-              <InputGroup labelText="Profession" />
               <InputGroup
+                register={register}
+                name="profession"
+                errors={errors.profession}
+                labelText="Profession"
+              />
+              <InputGroup
+                register={register}
+                name="dob"
+                errors={errors.dob}
                 type="date"
                 labelText="Date of birth"
                 placeholder="DD/MM/YYYY"
               />
-            </div>
+              <InputGroup
+                register={register}
+                errors={errors.gender}
+                name="gender"
+                type="select"
+                labelText="Gender"
+                placeholder="What is your gender"
+                options={["Male", "Female", "Others - please specify below"]}
+              />
+            </form>
           </div>
         </SettingsLayout>
       </MainLayout>
