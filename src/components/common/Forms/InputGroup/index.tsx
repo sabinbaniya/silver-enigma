ba72ead93f0fefacfa3741921c4cec7c7e57e69c
sabinbaniya <dummymail@tutanota.de>
2023-04-13 19:34:41 +0545

    type: types improvment & pass register props

--- src/components/common/Forms/InputGroup/index.tsx
+++ src/components/common/Forms/InputGroup/index.tsx
@@ -1,21 +1,79 @@
 import { ReactNode } from "react";
+import type {
+  FieldError,
+  FieldValues,
+  Path,
+  UseFormRegister,
+} from "react-hook-form";
 import Input from "../Input";
 import Label from "../Label";
+import Select from "../Select";
 
-interface Props {
+interface DefaultProps<T extends FieldValues> {
   labelText: string;
   placeholder?: string;
   children?: ReactNode;
-  as?: "input" | "textarea";
-  type?: "text" | "date";
+  register: UseFormRegister<T>;
+  name: Path<T>;
+  errors: FieldError | undefined;
 }
 
-const InputGroup = ({ as, labelText, placeholder, children, type }: Props) => {
+type TypeTextProps = {
+  as?: "input";
+} & (TypeSelectProps | OtherTypesProps);
+
+interface TypeSelectProps {
+  type?: "select";
+  options: Array<string>;
+}
+
+interface OtherTypesProps {
+  type?: "text" | "date";
+  options?: never;
+}
+
+interface TypeTextareaProps {
+  as?: "textarea";
+  type?: never;
+  options?: never;
+}
+
+type Props<T extends FieldValues> = DefaultProps<T> &
+  (TypeTextProps | TypeTextareaProps);
+
+const InputGroup = <T extends FieldValues>({
+  as,
+  labelText,
+  placeholder,
+  children,
+  type,
+  options,
+  register,
+  name,
+  errors,
+}: Props<T>) => {
   return (
     <>
       <div className="my-6 flex flex-col space-y-1">
         <Label text={labelText} />
-        <Input type={type} as={as} placeholder={placeholder || labelText} />
+        {as === "textarea" ? (
+          <Input
+            as={as}
+            placeholder={placeholder || labelText}
+            register={register}
+            name={name}
+          />
+        ) : type === "select" ? (
+          <Select options={options} register={register} name={name} />
+        ) : (
+          <Input
+            type={type}
+            as={as}
+            placeholder={placeholder || labelText}
+            register={register}
+            name={name}
+          />
+        )}
         {children}
       </div>
     </>
