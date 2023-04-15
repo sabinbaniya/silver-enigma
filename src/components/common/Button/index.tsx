f7c7674af726da83cc988e7b3f04b84545c4a2d4
sabinbaniya <dummymail@tutanota.de>
2023-04-15 12:17:09 +0545

    feat: add `disabled` prop

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -6,6 +6,7 @@ interface Props {
   style: keyof typeof ButtonStyles;
   onClick?: Function;
   loading?: boolean;
+  disabled?: boolean;
   type?: "button";
 }
 
@@ -14,15 +15,24 @@ const ButtonStyles = {
   secondary: "bg-gray-100 text-gray-800",
 };
 
-const Button = ({ children, style, onClick, loading, type }: Props) => {
+const Button = ({
+  children,
+  style,
+  onClick,
+  loading,
+  type,
+  disabled,
+}: Props) => {
   return (
     <button
       type={type}
-      disabled={loading}
+      disabled={disabled || loading}
       onClick={() => onClick?.()}
       className={`relative rounded-lg p-2 px-4 font-semibold ${
         ButtonStyles[style]
-      } ${loading ? "cursor-wait opacity-80" : ""}`}
+      } ${loading ? "cursor-wait opacity-80" : ""} ${
+        disabled ? "cursor-not-allowed opacity-80" : ""
+      }`}
     >
       <span className={loading ? "opacity-0" : "opacity-100"}>{children}</span>
       <Loading
