cbbd573f4064749b2c5921b70480d9a9d2f60692
sabinbaniya <dummymail@tutanota.de>
2023-04-15 17:38:07 +0545

    feat: show projects based on users settings

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,31 +1,46 @@
+import { userPortfolioSettingsAtom } from "@/src/atoms/userPortfolioSettings";
+import { useAtom } from "jotai";
 import Grid from "../common/Grid";
 import SectionHeading from "../common/SectionHeading";
 import ProjectsCard from "./ProjectsCard";
 
 interface Props {}
 const ProjectsSection = ({}: Props) => {
+  const [userPortfolioSettings] = useAtom(userPortfolioSettingsAtom);
+
+  const shouldShow = (idx: number) =>
+    userPortfolioSettings?.projects.includes(idx);
+
   return (
     <>
       <section>
         <div className="my-8">
           <SectionHeading heading="Projects" action="Create new project" />
           <Grid>
-            <ProjectsCard
-              src="/assets/images/portfolio.png"
-              title="Personal Portfolio Website"
-            />
-            <ProjectsCard
-              src="/assets/images/portfolio.png"
-              title="Personal Portfolio Website"
-            />
-            <ProjectsCard
-              src="/assets/images/portfolio.png"
-              title="Personal Portfolio Website"
-            />
-            <ProjectsCard
-              src="/assets/images/portfolio.png"
-              title="Personal Portfolio Website"
-            />
+            {shouldShow(0) && (
+              <ProjectsCard
+                src="/assets/images/portfolio.png"
+                title="Personal Portfolio Website"
+              />
+            )}
+            {shouldShow(1) && (
+              <ProjectsCard
+                src="/assets/images/portfolio.png"
+                title="Personal Portfolio Website"
+              />
+            )}
+            {shouldShow(2) && (
+              <ProjectsCard
+                src="/assets/images/portfolio.png"
+                title="Personal Portfolio Website"
+              />
+            )}
+            {shouldShow(3) && (
+              <ProjectsCard
+                src="/assets/images/portfolio.png"
+                title="Personal Portfolio Website"
+              />
+            )}
           </Grid>
         </div>
       </section>
