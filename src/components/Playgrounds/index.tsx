98c6b1fbec8d382164abe49844b93e356a90b68b
sabinbaniya <dummymail@tutanota.de>
2023-04-15 17:37:57 +0545

    feat: show playgrounds based on users settings

--- src/components/Playgrounds/index.tsx
+++ src/components/Playgrounds/index.tsx
@@ -1,9 +1,15 @@
+import { userPortfolioSettingsAtom } from "@/src/atoms/userPortfolioSettings";
+import { useAtom } from "jotai";
 import Grid from "../common/Grid";
 import SectionHeading from "../common/SectionHeading";
 import PlaygroundsCard from "./PlaygroundsCard";
 
 interface Props {}
 const PlaygroundsSection = ({}: Props) => {
+  const [userPortfolioSettings] = useAtom(userPortfolioSettingsAtom);
+
+  const shouldShow = (idx: number) =>
+    userPortfolioSettings?.playgrounds.includes(idx);
   return (
     <>
       <section>
@@ -13,10 +19,18 @@ const PlaygroundsSection = ({}: Props) => {
             action="Create new playground"
           />
           <Grid>
-            <PlaygroundsCard src="/assets/icons/html-5.svg" />
-            <PlaygroundsCard src="/assets/icons/javascript.svg" />
-            <PlaygroundsCard src="/assets/icons/javascript.svg" />
-            <PlaygroundsCard src="/assets/icons/html-5.svg" />
+            {shouldShow(0) && (
+              <PlaygroundsCard src="/assets/icons/html-5.svg" />
+            )}
+            {shouldShow(1) && (
+              <PlaygroundsCard src="/assets/icons/javascript.svg" />
+            )}
+            {shouldShow(2) && (
+              <PlaygroundsCard src="/assets/icons/javascript.svg" />
+            )}
+            {shouldShow(3) && (
+              <PlaygroundsCard src="/assets/icons/html-5.svg" />
+            )}
           </Grid>
         </div>
       </section>
